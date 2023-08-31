2464a154b299eacdd241d49219e322bf433f815d
Adegunwa Toluwalope <toluwalope.adegunwa@sterling.ng>
2023-08-31 18:24:32 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+type Post = {
+  userId: string,
+  id: number,
+  title: string,
+  body: string
+}
+
+const instance = axios.create({
+  baseURL: process.env.BASE_URL || "https://jsonplaceholder.typicode.com",
+  timeout: 1000
+});
 
 function App() {
+  const [posts, setPost] = useState<Post[] | null>(null)
+  useEffect(()=>{
+    const url = '/posts'
+   instance(url).then(response=>{
+      console.log(response.data);
+      
+      setPost(response.data)
+    }).catch(err=>{
+      console.error(err);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {posts? posts.map(post=>{
+      return <div style={{border: '1px solid black', margin: '20px', padding: '10px'}} key={post.id}> {post.id}. {post.title} <p>{post.body}</p></div>
+     }): <p>No post</p>}
     </div>
   );
 }
